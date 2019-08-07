811bb54e0dd4d0134341a88474a591c2432f0f60
Fahad Ahmed <fahadahmed.2496@gmail.com>
2019-08-07 16:28:16 +0500

    Added course and degree

--- cms_system/models.py
+++ cms_system/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 
 
-class Student(models.Model):
+class Degree(models.Model):
     name = models.CharField(max_length=50)
-    roll_no = models.CharField(max_length=50)
-    degree = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    roll_no = models.CharField(max_length=50)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE)
+    course = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
+
+
+    
+
